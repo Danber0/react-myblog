82bc0cd90dfc9b337bddde47e65df51a8b6131af
Danber <bergauzen.danil@mail.ru>
2022-01-30 17:22:38 +0300

    fix img

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -115,7 +115,7 @@ export const Article: React.FC<ArticleProps> = ({
                         </div>
                         <div className="article__image">
                           <img
-                            src={`/${post.photoUrl || "/uploads/no-image.png"}`}
+                            src={`${post.photoUrl || "/uploads/no-image.png"}`}
                             alt="img"
                           />
                         </div>
